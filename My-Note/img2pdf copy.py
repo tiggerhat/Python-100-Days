f72b30e847e99949cbcb2f50e51b5bfafec99b28
talentpanda <cumthyj@outlook.com>
2023-03-14 11:07:01 +0800

    vault backup: 2023-03-14 11:07:01

---
+++ My-Note/img2pdf copy.py
@@ -0,0 +1,30 @@
+import os
+from PIL import Image
+from io import BytesIO
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+# 定义文件夹路径和PDF文件名
+folder_path = 'C:\\Users\\cumth\\Pictures\\xw-excrise'
+pdf_file_name = 'merged.pdf'
+
+# 获取文件夹中的所有图片
+image_files = [f for f in os.listdir(folder_path) if f.endswith('.jpg') or f.endswith('.png')]
+
+# 创建一个PdfFileWriter对象
+pdf_writer = PdfFileWriter()
+
+# 遍历所有图片并将其添加到PdfFileWriter对象中
+for image_file in image_files:
+    # 打开图片文件并将其转换为PDF格式
+    image = Image.open(os.path.join(folder_path, image_file))
+    with BytesIO() as f:
+        image.save(f, format='PDF')
+        pdf_bytes = f.getvalue()
+    # 将PDF字节流添加到PdfFileWriter对象中
+    pdf_reader = PdfFileReader(BytesIO(pdf_bytes))
+    for page in range(pdf_reader.getNumPages()):
+        pdf_writer.addPage(pdf_reader.getPage(page))
+
+# 将所有PDF合并为一个文件
+with open(pdf_file_name, 'wb') as f:
+    pdf_writer.write(f)
